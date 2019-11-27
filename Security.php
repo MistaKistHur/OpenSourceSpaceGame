8e2e2a0e814b8a9863037b8dece64c085f8cc036
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-11-27 00:23:55 +0000

    Update Security.php

--- Security.php
+++ Security.php
@@ -1,4 +1,30 @@
 <?php
+// Learn More how to use these properly.
+// ::>>. Notes:
+// ::>>. Contact: Kik: Kaliphos287....
+
+public static function init(){
+
+}
+public static fuunction db(){
+
+}
+
+private protected function(){
+
+}
+
+private protected function(){
+
+}
+
+protected static function(){
+
+}
+protected static void function(){
+
+}
+
 
 		private $Uni_Hash_Cookie  = bin2hex(128);
 		private $Uni_Space_Cookie = sha512(32);
