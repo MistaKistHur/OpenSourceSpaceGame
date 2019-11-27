470e8582be397a28b899317dac9fc1d0e0835ceb
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-11-27 00:17:39 +0000

    Template

--- Security.php
+++ Security.php
@@ -1,1 +1,12 @@
+<?php
 
+		private $Uni_Hash_Cookie  = bin2hex(128);
+		private $Uni_Space_Cookie = sha512(32);
+		private $Uni_Key_Code     = md5(16);
+
+		private   $usrCookie  = '',
+		private   $signDate   = '',
+		private   $email      = '',
+		private   $guid       = '',
+		private   $uid        = '',
+		private   $age        = '',
