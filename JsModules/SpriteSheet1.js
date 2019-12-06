18dbf7cf7e77b1c933e7b6ad01023f1731630fe4
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-12-06 19:36:28 +0000

    Create SpriteSheet1.js

---
+++ JsModules/SpriteSheet1.js
@@ -0,0 +1,10 @@
+export default class SpriteSheetO {                 // ::>> https://www.youtube.com/watch?v=g-FpDQ8Eqw8
+  constructor(img, width, height){
+  
+  }
+}
+define(name,x,y){
+
+}
+
+// ::>> needs to be imported into the main document or a service worker/controller. if using MVC.
