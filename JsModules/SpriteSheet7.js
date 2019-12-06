9c5b7a1d0d9cd9f65752fe142e7353c149cb5bbc
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-12-06 19:39:57 +0000

    Create SpriteSheet7.js

---
+++ JsModules/SpriteSheet7.js
@@ -0,0 +1,11 @@
+export default class SpriteSheetO {                 // ::>> https://www.youtube.com/watch?v=g-FpDQ8Eqw8
+  constructor(img, width, height){
+  
+  }
+}
+define(name,x,y){
+
+}
+
+// ::>> needs to be imported into the main document or a service worker/controller. if using MVC.
+// ::>> 8 in total[Should be plenty to get started.]
