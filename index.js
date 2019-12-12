a5b1175e6ba728cababcfe7d5665bc2fe993fc12
Ryan Tao Spencer Middleton <40926443+MistaKistHur@users.noreply.github.com>
2019-12-12 15:21:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+                                                      // ::>> Document Name := ''; V_numb := ''; Date := '';
+  // ::>>. Notes:
+"use-strict";
 
+import UiMain from 'Scripts/UiMain.js';
+import UiMenuOverlay from 'Scripts/UiMenuOv.js';
+import UsrBase from 'Scripts/UsrBaseModel.js';
+    // ::>> Use Mvc?
+
+  // ::>>. Notes:
+
+
+  // ::>>. Notes:
+
+  // ::>>. Notes:
+
+
+  // ::>>. Notes:
